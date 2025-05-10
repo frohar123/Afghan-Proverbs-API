0b03b4ba37dafa1e1960ea4f5ce1666785598a1f
Forohar <frohar.behekhwah@gmail.com>
2025-05-10 21:06:28 +0430

    conflicts solved

--- src/routes/proverbRoutes.js
+++ src/routes/proverbRoutes.js
@@ -10,16 +10,4 @@ router.post('/', proverbController.createProverb);
 router.put('/:id', proverbController.updateProverb);
 router.delete('/:id', proverbController.deleteProverb);
 
-import express from 'express';
-import * as proverbController from '../controllers/proverbControllers.js';
-
-const router = express.Router();
-
-router.get('/', proverbController.getProverbs);
-router.get('/random', proverbController.getRandomProverb);
-router.get('/:id', proverbController.getProverb);
-router.post('/', proverbController.createProverb);
-router.put('/:id', proverbController.updateProverb);
-router.delete('/:id', proverbController.deleteProverb);
-
 export default router;
--- src/utils/db.js
+++ src/utils/db.js
@@ -19,25 +19,4 @@ export const writeDB = async (data) => {
     } catch (error) {
         console.error('Error writing to DB:', error);
     }
-    import fs from 'fs/promises';
-    import path from 'path';
-
-    const dbPath = path.resolve('data', 'proverbs.json');
-
-    export const readDB = async () => {
-        try {
-            const data = await fs.readFile(dbPath, 'utf-8');
-            return JSON.parse(data);
-        } catch (error) {
-            console.error('Error reading DB:', error);
-            return { proverbs: [] };
-        }
-    };
-
-    export const writeDB = async (data) => {
-        try {
-            await fs.writeFile(dbPath, JSON.stringify(data, null, 2));
-        } catch (error) {
-            console.error('Error writing to DB:', error);
-        }
-    };
+};
