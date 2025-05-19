baa1d66936a28d285a79ef1350d5ab306536c4c5
Forohar <frohar.behekhwah@gmail.com>
2025-05-20 00:21:56 +0430

    Front's link added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import proverbRoutes from './routes/proverbRoutes.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use('/proverbs', proverbRoutes);
+
+const allowedOrigins = ['http://localhost:4200']
+app.use(cors({
+    origin: allowedOrigins
+}));
+app.use('/api/proverbs', proverbRoutes);
 
 app.get('/', (req, res) => {
     res.send('Afghan Proverbs API');
